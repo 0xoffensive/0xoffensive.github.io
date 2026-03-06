eddd32fd95843160d0d6ae0bde28bdd4bc8de193
0xoffensive <159966765+0xoffensive@users.noreply.github.com>
2026-03-06 22:06:59 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export", // Enable static export
-  images: {
-    unoptimized: true, // Disable image optimization for static export
-  },
 };
 
 export default nextConfig;
